ffbdad4e260d4cb2eab4c00ea9088053b429a537
Aliyar F <aliyarfatemi@gmail.com>
2018-03-01 05:15:36 +0000

    Revesion for AS6-Seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,6 +25,10 @@
  Post.find_or_create_by(title: "A unique title", body: "A unique body")
  puts "#{Post.count}"
 
+ puts "#{Comment.count}"
+ Comment.find_or_create_by(body:"A unique body for comment")
+ puts "#{Comment.count}"
+ 
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
