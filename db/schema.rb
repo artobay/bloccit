f712cc98eff04e31c5cf2d093d592a8cd63e4d51
Aliyar F <aliyarfatemi@gmail.com>
2018-03-10 18:32:59 +0000

    SponsoredPost is completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180309201904) do
-
-  create_table "advertisements", force: :cascade do |t|
-    t.string "title"
-    t.text "copy"
-    t.integer "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20180310172929) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -36,13 +28,15 @@ ActiveRecord::Schema.define(version: 20180309201904) do
     t.integer "topic_id"
     t.index ["topic_id"], name: "index_posts_on_topic_id"
   end
-#note : topic_id is a column to the posts table with index 
-  create_table "questions", force: :cascade do |t|
+
+  create_table "sponsored_posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.boolean "resolved"
+    t.integer "price"
+    t.integer "topic_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["topic_id"], name: "index_sponsored_posts_on_topic_id"
   end
 
   create_table "topics", force: :cascade do |t|
