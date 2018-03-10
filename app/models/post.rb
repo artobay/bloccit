8cb77836ddf4ec7cd823f6714e8d26dae5d71c2f
Aliyar F <aliyarfatemi@gmail.com>
2018-03-10 22:07:16 +0000

    form validation checkpoint is completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ApplicationRecord
      belongs_to :topic 
      has_many :comments, dependent: :destroy
+
+#note: models can filter our database, we are going to filter our db by lenght and presence
+
+   validates :title, length: { minimum: 5 }, presence: true
+   validates :body, length: { minimum: 20 }, presence: true
+   validates :topic, presence: true
+
+
 end
