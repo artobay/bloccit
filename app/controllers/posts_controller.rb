54ff69c39279a5134a118415978d64241783dd95
Aliyar F <aliyarfatemi@gmail.com>
2018-03-17 18:21:49 +0000

    checkpoint 26 posts and users is completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   
-
+before_action :require_sign_in, except: :show
   def show
       @post = Post.find(params[:id])
   end
@@ -11,13 +11,11 @@ class PostsController < ApplicationController
   end
   
   def create
-
-     @post = Post.new
-     @post.title = params[:post][:title]
-     @post.body = params[:post][:body]
      @topic = Topic.find(params[:topic_id])
-     @post.topic = @topic
-
+     
+     @post = @topic.posts.build(post_params)
+     
+     @post.user = current_user
      if @post.save
 
        flash[:notice] = "Post was saved."
@@ -34,9 +32,9 @@ class PostsController < ApplicationController
   end
   
   def update
-     @post = Post.find(params[:id])
-     @post.title = params[:post][:title]
-     @post.body = params[:post][:body]
+      @topic = Topic.find(params[:topic_id])
+      @post = Post.find(params[:id])
+      @post.assign_attributes(post_params)
  
      if @post.save
        flash[:notice] = "Post was updated."
@@ -60,5 +58,10 @@ class PostsController < ApplicationController
      end
   end
 
+private 
+
+def post_params
+params.require(:post).permit(:title, :body)
+end 
 
 end 
