b3be8db2b6c49b8948b46fe7e5ba5ea271480555
Aliyar F <aliyarfatemi@gmail.com>
2018-03-22 00:21:51 +0000

    Voting Checkpoint is completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,12 +2,33 @@ class Post < ApplicationRecord
      belongs_to :topic 
      belongs_to :user
      has_many :comments, dependent: :destroy
-       default_scope { order('created_at DESC') }
+       default_scope { order('rank DESC') }
 #note: models can filter our database, we are going to filter our db by lenght and presence
-
+     has_many :votes, dependent: :destroy
+   
    validates :title, length: { minimum: 5 }, presence: true
    validates :body, length: { minimum: 20 }, presence: true
    validates :topic, presence: true
    validates :user, presence: true
-
+   
+   def up_votes
+ # #9 call count to get a total of all up votes 
+     votes.where(value: 1).count
+   end
+ 
+   def down_votes
+ # #10
+     votes.where(value: -1).count
+   end
+ 
+   def points
+ # #11
+     votes.sum(:value)
+   end
+   
+   def update_rank
+     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
+     new_rank = points + age_in_days
+     update_attribute(:rank, new_rank)
+   end
 end
